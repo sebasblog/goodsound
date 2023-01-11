6fc278daa159ff08e2cecc66f92bc045cb06b973
Sebastian Pernia <sebaspernia@gmail.com>
2023-01-11 20:13:22 +0100

    Ajustando proyecto

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 "use strict"
 
+function irArriba(){
+    window.addEventListener('scroll', () =>{
+        let scroll = document.documentElement.scrollTop;
+        console.log(scroll);
+        let flechaArriba = document.getElementById('flechaArriba');
+
+        if(scroll > 300) {
+            flechaArriba.style.right = 20 + "px";
+        } else {
+            flechaArriba.style.right = -140 + "px";
+        }
+    })
+}
+
+irArriba();
